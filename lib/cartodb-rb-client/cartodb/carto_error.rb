80c2a4098df4e18bff7ec07cbddfe73e0ffbd4dd
Fernando Espinosa <fer@ferdev.com>
2011-03-14 16:05:00 +0100

    Returns nil when requesting a table which doesn't exist

--- lib/cartodb-rb-client/cartodb/carto_error.rb
+++ lib/cartodb-rb-client/cartodb/carto_error.rb
@@ -6,6 +6,8 @@ module CartoDB
       @method         = method
       @error_messages = ['undefined CartoDB error']
       @status_code    = 400
+      @error_messages = [uri] if method == nil && http_response == nil
+
 
       if http_response
         @status_code = http_response.code
@@ -14,12 +16,24 @@ module CartoDB
 
     end
 
+    def initialize(error_message)
+      @error_messages = [error_message]
+    end
+
     def to_s
       <<-EOF
-        There were errors running the #{@method.upcase} request "#{@uri}":
+        #{http_error_message_header}
         #{format_error_messages}
       EOF
+      .strip
     end
+
+    def http_error_message_header
+      if @method && @uri
+        %{There were errors running the #{@method.upcase} request "#{@uri}":}
+      end
+    end
+    private :http_error_message_header
 
     def custom_error(http_response)
       json = Utils.parse_json(http_response)
