fecd295e622339164ace5f005321d3a5791ea9ad
Fernando Blat <ferblape@gmail.com>
2011-04-04 09:05:08 +0200

    Accept :page parameter in records method to paginate results

--- lib/cartodb-rb-client.rb
+++ lib/cartodb-rb-client.rb
@@ -3,6 +3,7 @@ require 'oauth'
 require 'typhoeus'
 require 'mime/types'
 require 'json'
+require 'active_support/core_ext/hash/slice'
 
 require 'cartodb-rb-client/cartodb'
 require 'cartodb-rb-client/railtie' if defined?(Rails)
