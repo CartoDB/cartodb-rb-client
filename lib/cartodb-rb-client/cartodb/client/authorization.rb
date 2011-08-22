dcf5844bb6b36232000d553cbc04e0475dfda418
Fernando Espinosa <fer@ferdev.com>
2011-08-23 00:41:13 +0200

    Allows to connect with CartoDB using username/password or access tokens

--- lib/cartodb-rb-client/cartodb/client/authorization.rb
+++ lib/cartodb-rb-client/cartodb/client/authorization.rb
@@ -16,7 +16,31 @@ module CartoDB
       private :signed_request
 
       def access_token
-        @access_token ||= OAuth::AccessToken.new(oauth_consumer, CartoDB::Settings['oauth_access_token'], CartoDB::Settings['oauth_access_token_secret'])
+        return @access_token if @access_token
+
+        @access_token ||= if CartoDB::Settings['oauth_access_token'] && CartoDB::Settings['oauth_access_token_secret']
+          OAuth::AccessToken.new(oauth_consumer, CartoDB::Settings['oauth_access_token'], CartoDB::Settings['oauth_access_token_secret'])
+        elsif CartoDB::Settings['username'] && CartoDB::Settings['password']
+          access_token_url = oauth_consumer.access_token_url
+
+          request = Typhoeus::Request.new(access_token_url,
+            :method => :post,
+            :params => {:x_auth_mode => :client_auth, :x_auth_username => CartoDB::Settings['username'], :x_auth_password => CartoDB::Settings['password']}
+          )
+
+          helper = OAuth::Client::Helper.new(request, {:consumer => oauth_consumer, :request_uri => access_token_url})
+
+          request.headers.merge!({"Authorization" => helper.header})
+
+          @hydra.queue request
+          @hydra.run
+
+          values = request.response.body.split('&').inject({}) { |h,v| h[v.split("=")[0]] = v.split("=")[1]; h }
+
+          OAuth::AccessToken.new(oauth_consumer, values["oauth_token"], values["oauth_token_secret"])
+        else
+          nil
+        end
       end
       private :access_token
 
